c41df7c821cecac6bdc55665345e61e512853912
sahithyabeesu03 <sahithyabeesu0302@gmail.com>
2026-03-30 16:53:19 +0530

    added a simple line

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,17 @@
 package org.example;
 
+
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+    public static int sum(int a,int b){
+        return a+b;
+    }
     public static void main(String[] args) {
+
+
+
 
         for(int i=0;i<10;i++){
             System.out.println("Hello");
@@ -12,7 +20,9 @@ public class Main {
         System.out.println("everyone");
         System.out.println("hiii");
 
-
+   int a=10;
+   int b=20;
+   System.out.println(sum(a,b));
 
 
     }
